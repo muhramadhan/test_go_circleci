7b9a57158dbad1ffb8cf8e2417ef1a0a63063cdd
nakama <nakama@202751.local>
2019-06-12 22:46:57 +0700

    fail test

--- server_test.go
+++ server_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestMessage(t *testing.T) {
     msg := message()
-    if msg != "Hello World" {
+    if msg != "Hello Worlddddd" {
        t.Errorf("Message was incorrect, got: %s, want: %s.", msg, "Hello World")
     }
 }

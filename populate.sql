2742848d2d5b772d783765238b81b0e1c0c13ede
muhramadhan <ramadhanm1998@gmail.com>
2019-06-14 16:19:44 +0700

    circleci

--- populate.sql
+++ populate.sql
@@ -11,40 +11,22 @@ CREATE DATABASE demo_circleci
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-
-
--- SCHEMA: public
-
--- DROP SCHEMA public ;
-
-CREATE SCHEMA public
-    AUTHORIZATION postgres;
-
-COMMENT ON SCHEMA public
-    IS 'standard public schema';
-
-GRANT ALL ON SCHEMA public TO postgres;
-
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
-
-INSERT INTO public."User"(
+INSERT INTO "User"(
 	nama, email)
 	VALUES ('aaaa', 'aaaa@gmail.com');
 	
-INSERT INTO public."User"(
+INSERT INTO "User"(
 	nama, email)
 	VALUES ('bbbb', 'bbbb@gmail.com');
 
-INSERT INTO public."User"(
+INSERT INTO "User"(
 	nama, email)
 	VALUES ('cccc', 'cccc@gmail.com');
 
-INSERT INTO public."User"(
+INSERT INTO "User"(
 	nama, email)
 	VALUES ('dddd', 'dddd@gmail.com');
 
-INSERT INTO public."User"(
+INSERT INTO "User"(
 	nama, email)
 	VALUES ('eeee', 'eeee@gmail.com');

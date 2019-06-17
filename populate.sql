3f1419aa3df7dbf8bafff7e0c8a2930d99c145da
muhramadhan <ramadhanm1998@gmail.com>
2019-06-17 09:41:53 +0700

    try circleci

--- populate.sql
+++ populate.sql
@@ -14,6 +14,21 @@ CREATE DATABASE demo_circleci
 
 \c demo_circleci
 
+-- SCHEMA: public
+
+-- DROP SCHEMA public ;
+
+CREATE SCHEMA public
+    AUTHORIZATION postgres;
+
+COMMENT ON SCHEMA public
+    IS 'standard public schema';
+
+GRANT ALL ON SCHEMA public TO postgres;
+
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
 INSERT INTO public."User"(
 	nama, email)
 	VALUES ('aaaa', 'aaaa@gmail.com');
